af95a99c164f3ba90e50fb4bd3f165ff5102bd2e
L-fernando345 <macariofernando105@gmail.com>
2026-04-16 17:12:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,36 +7,40 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+</head>
+        <body class="bg-dark">
+            <header class="bg-danger text-white">
+                <h1 class="text-color "><img src="img/UmbrellaCorp.png" alt="Logo" class="img-fluid">Umbrella Gamer Store</h1>
 <?php
 session_start();
 if (isset($_SESSION['user_id'])) {
-    echo "<span class='bg-danger text-white'>Olá, {$_SESSION['user_nome']}! </span>";
+    echo "<span class='bg-danger text-white'><h3>Olá, {$_SESSION['user_nome']}! </h3></span>";
     // echo "<a href='logout.php'>Sair</a>";
 // } else {
 //     echo "<a href='login.php'>Login?Cadastro</a>";
 }
 ?>
-</head>
-        <body class="bg-dark">
-            <header class="bg-danger text-white">
-                <h1 class="text-color "><img src="img/UmbrellaCorp.png" alt="Logo" class="img-fluid">Umbrella Gamer Store</h1>
                 <nav class="navbar navbar-lg">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
-    
-
-<div class="collapse navbar-collapse" id="navbarNav">
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarNav" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
       <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php"><h3>Home</h3></a></li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="carrinho.php">Carrinho</a>
+          <a class="nav-link active" aria-current="page" href="carrinho.php"><h3>Carrinho</h3></a>
         </li>
         <?php if(isset($_SESSION['user_id'])): ?>
-            <li class="nav-item"><a class="nav-link active" href="logout.php" class="btn-sair">Sair</a></li>
+            <li class="nav-item"><a class="nav-link active" href="logout.php" class="btn-sair"><h3>Sair</h3></a></li>
         <?php else: ?>
-            <li class="nav-item"><a class="nav-link active" aria-current="page" href="login.php">Login</a></li>
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="login.php"><h3>Login</h3></a></li>
         <?php endif; ?>
+        <?php if (isset($_SESSION['user_nivel']) && $_SESSION['user_nivel'] == 'admin'): ?>
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="admin.php"><h3>Painel Admin</h3></a></li>
+        <?php endif; ?>
+
       </ul>
 </div>
 </nav>
