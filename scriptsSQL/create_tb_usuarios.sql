138ab58251df69b3b9225678d9ddda63c0edd959
Karlus Kilderi Lopes <karluslopes@edu.ce.senac.br>
2026-04-22 16:51:36 -0300

    Inserção da coluna "ativo" na tabela de usuários

--- scriptsSQL/create_tb_usuarios.sql
+++ scriptsSQL/create_tb_usuarios.sql
@@ -3,6 +3,7 @@ CREATE TABLE usuarios (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
+    ativo TINYINT(1) DEFAULT 1,
     nivel ENUM('cliente', 'admin', 'adminP') DEFAULT 'cliente',
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
