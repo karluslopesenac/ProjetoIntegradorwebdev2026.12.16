6dcfffbdfca97b844b6fea7235215a4a769c41b2
Karlus Kilderi Lopes <karluslopes@edu.ce.senac.br>
2026-04-23 15:49:45 -0300

    Adicionado script para criação do usuário admin

---
+++ scriptsSQL/create_admin_user.sql
@@ -0,0 +1,5 @@
+-- Criar admin padrão (senha: admin2026)
+INSERT INTO admins (usuario, email, senha) VALUES (
+    'admin',
+    'admin@lojagamer.com',
+    '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi');
